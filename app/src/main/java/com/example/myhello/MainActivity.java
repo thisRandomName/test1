e2050ec8cf5188878f8165e6bb552b7dbfe94cab
mina <minakopfot@gmail.com>
2021-02-18 08:37:28 +0200

    add mainactivity2

--- app/src/main/java/com/example/myhello/MainActivity.java
+++ app/src/main/java/com/example/myhello/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.myhello;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +17,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        Button click = (Button) findViewById(R.id.button);
+
+        click.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                    Toast.makeText(MainActivity.this, "Peinaw", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+         });
+    }
+
 }
